15002dd2661a1345943e4a26073f33a7fcb88e80
Rutger <rutgerdj2@gmail.com>
2022-06-01 14:27:43 +0200

    Add basic styling for start menu

--- frontend/src/app/components/start/expandmenu/expandmenu.js
+++ frontend/src/app/components/start/expandmenu/expandmenu.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./expandmenu.css";
 import $ from "jquery";
+import logoff from "images/start/logoff.png";
+import shutoff from "images/start/shutoff.png";
 
 export default class ExpandMenu extends React.Component {
   toggle() {
@@ -15,8 +17,32 @@ export default class ExpandMenu extends React.Component {
   render() {
     return (
       <div id="expandStartMenu" ref={(m) => (this.menu = m)}>
-        <div>start</div>
-        <div>start</div>
+        <div className="topsection">
+          <span>
+            Epic Gamer
+          </span>
+        </div>
+        <div>
+          <div className="column left">
+          Left
+          </div>
+          <div className="column right">
+          Right
+          </div>
+        </div>
+        <div className="bottomsection">
+          <div className="bottomButtons">
+          <div>
+            <img id="logoffbutton" width="30" src={logoff} alt="" />
+            <span>Log off</span>
+          </div>
+
+          <div>
+            <img id="shutoffbutton" width="30" src={shutoff} alt="" />
+            <span>Shut Down</span>
+          </div>
+          </div>
+        </div>
       </div>
     );
   }
