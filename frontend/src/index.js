db92c17ab586a416497e813a2efcceca97b74a50
Rutger <rutgerdj2@gmail.com>
2022-06-20 13:03:47 +0200

    Add CSS normalizer

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./app/app/App";
 import "xp.css/dist/XP.css";
+import "normalize.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
